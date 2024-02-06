0b9b397f910a0a734d1b33341b4714472f145ed3
Romeo <104751985+semklim@users.noreply.github.com>
2024-02-06 11:18:10 +0200

    fix

--- api/index.js
+++ api/index.js
@@ -38,6 +38,7 @@ app.use('/api/bill', billRoutes);
 app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/public')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
